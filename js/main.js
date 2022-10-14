2a052a42e2c674a169833bc11e995e7daf3be918
k2sebeom <moses97@gmail.com>
2022-10-14 14:32:37 -0400

    feat: Change bio and profile

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Software Development.", "Physical Simulation.", "Artificial Intelligence.", "Robotics."],
+      strings: ["Software Development.", "Game Development.", "Cloud Computing.", "Artificial Intelligence.", "Blockchain."],
       typeSpeed: 100,
       loop: true,
     });
