81dfabae81336357bb48090130c343d27e17082b
k2sebeom <moses97@gmail.com>
2023-04-16 14:17:01 -0400

    feat: Add grid structure

--- src/components/layouts/SectionContainer.tsx
+++ src/components/layouts/SectionContainer.tsx
@@ -6,6 +6,8 @@ const SectionContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
+
+  /* background-color: aqua; */
 `;
 
 export default SectionContainer;
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
 import SectionContainer from '../layouts/SectionContainer';
 import { SectionTitle } from '../shared/Typography';
+import styled from 'styled-components';
+
+const ProjectsContainer = styled.div`
+  /* background-color: pink; */
+
+  width: 90%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* box-sizing: border-box; */
+  /* padding-left: 1rem; */
+  /* padding-right: 1rem; */
+`;
+
+const ProjectsGrid = styled.div`
+  display: grid;
+
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 1rem;
+
+  width: 100%;
+
+  & h1 {
+    text-align: center;
+    /* background-color: blue; */
+  }
+`;
 
 function Projects(): JSX.Element {
   return (
     <SectionContainer>
-      <div>
+      <ProjectsContainer>
         <SectionTitle>Projects</SectionTitle>
-      </div>
+        <ProjectsGrid>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((e, i) => {
+            return <h1 key={`foo-${i}`}>Foo {i}</h1>;
+          })}
+        </ProjectsGrid>
+      </ProjectsContainer>
     </SectionContainer>
   );
 }
