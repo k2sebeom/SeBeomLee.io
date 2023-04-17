928ce76742d366113bc4983b62559b51ecb032a8
k2sebeom <moses97@gmail.com>
2023-04-16 21:17:19 -0400

    feat: Add dark mode shadowa

--- src/components/projects/ProjectItem.tsx
+++ src/components/projects/ProjectItem.tsx
@@ -4,13 +4,12 @@ import { type ProjectInfo } from '../../data/projectsData';
 
 const ItemContainer = styled.div`
   border-radius: 1rem;
-  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+  box-shadow: ${(props) => props.theme.colors.shadowPrimary};
 
   transition: box-shadow 0.5s ease;
 
   &:hover {
-    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
-      rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+    box-shadow: ${(props) => props.theme.colors.shadowSecondary};
   }
 `;
 
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -7,6 +7,10 @@ const themes = {
       textColor: '#FFF',
 
       labelColor: '#FFF',
+
+      shadowPrimary: 'rgba(255, 255, 255, 0.1) 0px 4px 12px',
+      shadowSecondary:
+        'rgba(255, 255, 255, 0.25) 0px 54px 55px, rgba(255, 255, 255, 0.12) 0px -12px 30px, rgba(255, 255, 255, 0.12) 0px 4px 6px, rgba(255, 255, 255, 0.17) 0px 12px 13px, rgba(255, 255, 255, 0.09) 0px -3px 5px',
     },
   },
   light: {
@@ -17,6 +21,10 @@ const themes = {
       textColor: '#FFF',
 
       labelColor: '#000',
+
+      shadowPrimary: 'rgba(0, 0, 0, 0.1) 0px 4px 12px',
+      shadowSecondary:
+        'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px',
     },
   },
 };
