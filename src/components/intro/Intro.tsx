88d5a504b9cc5b94a09bbed24c09ff995b5bac2f
k2sebeom <moses97@gmail.com>
2023-04-19 16:33:48 -0400

    feat: Add responsive to intro

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import IntroEarth from './IntroEarth';
 import IntroTyping from './IntroTyping';
+import { breakpoints } from '../../styles/theme';
 
 const IntroContainer = styled.div`
   position: relative;
+
+  @media (max-width: ${breakpoints.xs}) {
+    margin-bottom: 5rem;
+  }
 `;
 
 function Intro(): JSX.Element {
